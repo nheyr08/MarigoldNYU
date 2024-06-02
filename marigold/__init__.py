aadfd782f712a5257acda91df6bf596d29fa40c7
beatsmasterz <henrybetsaleel@gmail.com>
2024-06-03 03:43:59 +0800

    added comment

--- marigold/__init__.py
+++ marigold/__init__.py
@@ -18,4 +18,4 @@
 # --------------------------------------------------------------------------
 
 
-from .marigold_pipeline import MarigoldPipeline, MarigoldDepthOutput  # noqa: F401
+from .marigold_pipeline import MarigoldPipeline, MarigoldDepthOutput  # noqa: F401 this is the main entry point
